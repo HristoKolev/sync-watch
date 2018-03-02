3f0d14b536fa17bf96c69420aaaf40177db4f7d6
HristoKolev <hristo.a.kolev@gmail.com>
2018-03-02 22:31:39 +0200

    not working

---
+++ SyncWatch/SyncWatch/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SyncWatch
+{
+    using System;
+  
+
+    public class Program
+    {
+        private static void Main()
+        {
+            // Set up session options
+            SessionOptions sessionOptions = new SessionOptions
+            {
+                Protocol = Protocol.Sftp,
+                HostName = "vm3",
+                UserName = "root",
+                SshHostKeyFingerprint = "ssh-ed25519 256 M7hDz1mj12AngTbLl59DrCP+nnecAGtsudZsWVGmWCQ=",
+            };
+
+            using (Session session = new Session())
+            {
+                // Connect
+                session.Open(sessionOptions);
+
+                // Your code
+            }
+
+        }
+    }
+}
